8cfe1b35d3e82de19f9300284d7aa124b40cdee8
Johannes Brunner <johannes.brunner@world-direct.at>
2024-05-15 08:10:05 +0200

    call cosmos db

--- Function1.cs
+++ Function1.cs
@@ -2,9 +2,23 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
 namespace FunctionApp1
 {
+    public class CosmosDocument
+    {
+        [JsonPropertyName("msg")]
+        public string Msg { get; set; }
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+    }
+
     public class HttpExample
     {
         private readonly ILogger<HttpExample> _logger;
@@ -15,10 +29,14 @@ namespace FunctionApp1
         }
 
         [Function("HttpExample")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
+            [CosmosDBInput(databaseName: "my-database", containerName: "my-container", Connection  = "CosmosDbConnectionSetting", SqlQuery = "SELECT * from c")] IEnumerable<CosmosDocument> items)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            _logger.LogInformation("C# HTTP trigger function processed a request.");                        
+
+            var myObj = new { text = items.First().Msg };            
+
+            return new JsonResult(myObj);            
         }
     }
 }
